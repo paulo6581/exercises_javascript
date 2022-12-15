3758a1bd4ca493b6219802aea3ac1e06d4130a58
Paulo Roberto <p.roberto.portela21@gmail.com>
2022-12-15 11:33:16 -0300

    update list

--- estructure_listas/ex04.js
+++ estructure_listas/ex04.js
@@ -1,3 +1,14 @@
 // Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
 const input = require('prompt-sync')({sigint: true});
 let vetor = [];
+for (let i = 0; i < 10; i += 1) {
+    let caracter = input("Digite um caractere: ");
+    if(caracter.length === 1) {
+        vetor.push(caracter);
+    } else {
+        console.log("Digite somente um CARACTERE por vez!");
+        i -= 1;
+    }
+}
+const consoante = vetor.filter(valor => (valor != 'a' && valor != 'e' && valor != 'i' && valor != 'o' && valor != 'u'));
+console.log(consoante);
